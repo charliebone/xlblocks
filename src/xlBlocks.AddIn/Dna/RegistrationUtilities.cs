1099ab57b6e0ebafc34659f0d851694fda367a64
Charlie <cfriedem@gmail.com>
2025-01-04 12:29:02 -0500

    add optional arg info to registration name and description

---
+++ src/xlBlocks.AddIn/Dna/RegistrationUtilities.cs
@@ -0,0 +1,25 @@
+﻿namespace XlBlocks.AddIn.Dna;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using ExcelDna.Registration;
+
+internal static class RegistrationUtilities
+{
+    internal static IEnumerable<ExcelFunctionRegistration> EnrichRegistrations(this IEnumerable<ExcelFunctionRegistration> functionRegistrations)
+    {
+        foreach (var functionRegistration in functionRegistrations)
+        {
+            foreach (var parameterRegistration in functionRegistration.ParameterRegistrations)
+            {
+                if (parameterRegistration.CustomAttributes.OfType<OptionalAttribute>().Any() || parameterRegistration.CustomAttributes.OfType<DefaultParameterValueAttribute>().Any())
+                {
+                    parameterRegistration.ArgumentAttribute.Name = $"[{parameterRegistration.ArgumentAttribute.Name}]";
+                    parameterRegistration.ArgumentAttribute.Description = $"[Optional] {parameterRegistration.ArgumentAttribute.Description}";
+                }
+            }
+        }
+        return functionRegistrations;
+    }
+}
