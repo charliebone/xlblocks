661909ac073bedac85f10cc330d06c33fab7443d
charliebone <cfriedem@gmail.com>
2025-03-15 10:08:06 -0400

    Add pages (#40)
    add initial docgen and pages, still very much wip

---
+++ src/XlBlocks.AddIn.DocGen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using ExcelDna.Integration;
+using ExcelDna.Registration;
+using Grynwald.MarkdownGenerator;
+using XlBlocks.AddIn.Dna;
+
+// generate documentation based on excel functions
+Console.WriteLine("Starting doc gen...");
+
+var addInAssembly = Assembly.GetAssembly(typeof(XlBlocksAddIn)) ?? throw new Exception("cannot find AddIn assembly");
+
+var excelFunctions = addInAssembly.GetTypes()
+    .SelectMany(x => x.GetMethods(BindingFlags.Public | BindingFlags.Static))
+    .Where(x => x.GetCustomAttribute<ExcelFunctionAttribute>() is not null)
+    .Where(x => x.GetCustomAttribute<IntegrationTestExcelFunctionAttribute>() is null)
+    .Select(x => new ExcelFunctionRegistration(x));
+
+var functionsByType = excelFunctions
+    .GroupBy(x => x.FunctionAttribute.Name.Split("_")[0]);
+
+var excelDocSet = new DocumentSet<MdDocument>();
+foreach (var functionGroup in functionsByType)
+{
+    // each group gets its own page
+    var trimmedKey = functionGroup.Key.Replace("XB", "");
+    var groupDocument = excelDocSet.CreateMdDocument($"{trimmedKey.ToLower()}.md");
+    groupDocument.Root.Add(new MdHeading(1, trimmedKey));
+}
+
+// save to /docs/excel folder
+excelDocSet.Save("../../../../docs/excel");
+Console.WriteLine("Doc gen is finished.");
