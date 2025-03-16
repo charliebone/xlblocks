e35db076d40f27f231e82826132524524cdf6a7e
Charlie <cfriedem@gmail.com>
2025-03-16 08:55:58 -0400

    specify output path to docgen

--- src/XlBlocks.AddIn.DocGen/Program.cs
+++ src/XlBlocks.AddIn.DocGen/Program.cs
@@ -2,13 +2,21 @@
 using ExcelDna.Integration;
 using ExcelDna.Registration;
 using Grynwald.MarkdownGenerator;
+using Microsoft.Extensions.Configuration;
 using XlBlocks.AddIn.Dna;
 
 // generate documentation based on excel functions
 Console.WriteLine("Starting doc gen...");
 
-var addInAssembly = Assembly.GetAssembly(typeof(XlBlocksAddIn)) ?? throw new Exception("cannot find AddIn assembly");
+var config = new ConfigurationBuilder()
+    .AddCommandLine(args)
+    .Build();
 
+var outputPath = config["OutputPath"] ?? "../../../../docs/docs/excel";
+if (!Directory.Exists(outputPath))
+    throw new Exception($"Output path '{outputPath}' does not exist");
+
+var addInAssembly = Assembly.GetAssembly(typeof(XlBlocksAddIn)) ?? throw new Exception("cannot find AddIn assembly");
 var excelFunctions = addInAssembly.GetTypes()
     .SelectMany(x => x.GetMethods(BindingFlags.Public | BindingFlags.Static))
     .Where(x => x.GetCustomAttribute<ExcelFunctionAttribute>() is not null)
@@ -28,5 +36,5 @@ foreach (var functionGroup in functionsByType)
 }
 
 // save to /docs/excel folder
-excelDocSet.Save("../../../../docs/docs/excel");
+excelDocSet.Save(outputPath);
 Console.WriteLine("Doc gen is finished.");
